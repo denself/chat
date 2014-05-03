1f3d11ee43eaf5202851fd907150bddcf4bca354
denself <denself@gmail.com>
2014-05-03 18:50:50 +0300

    1

---
+++ app.py
@@ -0,0 +1,33 @@
+__author__ = 'denself'
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+
+clients = []
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(request):
+        print "get"
+        request.render("index.html")
+
+
+class WebSocketChatHandler(tornado.websocket.WebSocketHandler):
+    def open(self, *args):
+        print("open", "WebSocketChatHandler")
+        clients.append(self)
+
+    def on_message(self, message):
+        print message
+        for client in clients:
+            client.write_message(message)
+
+    def on_close(self):
+        clients.remove(self)
+
+
+app = tornado.web.Application([(r'/chat', WebSocketChatHandler), (r'/', IndexHandler)])
+
+app.listen(80)
+tornado.ioloop.IOLoop.instance().start()
